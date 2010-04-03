c1f4de013722b5cfa82a7913c3cbaa534d549ced
trapd00r <trapd00r@trapd00r.se>
2010-04-03 10:33:12 +0200

    Added interactive mode

--- mail.pl
+++ mail.pl
@@ -1,18 +1,31 @@
 #!/usr/bin/perl
-#mail
 use strict;
+#mail
 
-if(!@ARGV) {
-  print STDERR "$0 <subject> <body> <email>\n";
-  exit 1;
-}
 my @details = @ARGV;
 my $acc = "gmail";
+
+&interactive if !@ARGV;
 
 sub mail {
   my ($subject, $body, $rec) = @_;
   $rec = "trapd00r\@trapd00r.se" unless $rec;
   my $content = sprintf("Subject:$subject\n\n$body\n");
   system("printf \"$content\"|msmtp -a $acc \"$rec\"");
+  print "Status: $?\n";
 }
 &mail(@details);
+
+sub interactive {
+  print "Subject: ";
+  my $subject = <STDIN>;
+  print "Body: ";
+  my $body    = <STDIN>;
+  print "Mail: ";
+  my $rec     = <STDIN>;
+  chomp($subject,$body,$rec);
+  my $content = sprintf("Subject:$subject\n$body\n");
+  system("printf \"$content\"|msmtp -a $acc \"$rec\"");
+  print "Status: $?\n";
+}
+
