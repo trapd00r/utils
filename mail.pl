3fcb3d85692ecc32185939e3dfddbbe56e5fab3c
trapd00r <trapd00r@trapd00r.se>
2010-04-04 11:27:56 +0200

    Added readStdin so I could do things like cat foo.pl|mail -stdin

--- mail.pl
+++ mail.pl
@@ -1,12 +1,33 @@
 #!/usr/bin/perl
 use strict;
 #mail
+use Getopt::Long;
+
+&interactive if !@ARGV;
+our $opt_stdin;
+GetOptions(stdin  =>  \$opt_stdin);
 
 my @details = @ARGV;
 my $acc = "gmail";
+my $def_Rec = "trapd00r\@trapd00r.se";
 
-&interactive if !@ARGV;
 
+&readStdin if $opt_stdin;
+
+sub readStdin {
+  my @body;
+  while(<>) {
+    push(@body, $_);
+  }
+  my $body = join/\n/, @body;
+  my $rec  = $def_Rec;
+  my $subject = 'stdin';
+  my $content = sprintf("Subject:$subject\n\n$body\n");
+  system("printf \"$content\"|msmtp -a $acc \"$def_Rec\"");
+  print "Status: $?\n";
+  exit 0;
+}
+  
 sub mail {
   my ($subject, $body, $rec) = @_;
   $rec = "trapd00r\@trapd00r.se" unless $rec;
