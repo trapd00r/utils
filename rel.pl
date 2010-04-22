c4441d3d6640425cc8cb8b938a2e6d47a52cfd23
trapd00r <trapd00r@trapd00r.se>
2010-04-22 10:53:40 +0200

    Total rewrite. Fixed width, truncate long ass relnames, use descriptions on the right.

--- rel.pl
+++ rel.pl
@@ -13,7 +13,7 @@ my (@episodes, @floss);
 foreach my $release(@releases) {
   next unless($release =~ /Downloading:/);
   $release =~ s/(?:\w+\s+){2}\w+: //;
-  push(@episodes, $release) if $release =~ /(S[0-9]+)?(E[0-9]+)?(.*TV)/;
+  push(@episodes, $release) if $release =~ /(S[0-9]+)?(E[0-9]+)?(.*TV)|VHS/;
   push(@floss, $release) if $release =~ /FLOSS/;
 }
 
@@ -27,18 +27,27 @@ my $reSport     = 'EPL|WWE|UFC|UEFA|Rugby|La\.Liga|Superleague
                    |Allsvenskan|Formula\.Ford';
 my $reSwe       = 'swedish';
 
+my $colorNone   = "\033[0m";
+my $colorWanted = "\033[38;5;202m";
+my $suffixNew   = "\033[38;5;196mNew Show\033[0m";
+my $suffixDocu  = "\033[38;5;100mDocumentary\033[0m";
+my $suffixSwe   = "\033[38;5;44mSwedish\033[0m";
+my $suffixSport = "\033[38;5;144mSport\033[0m";
+my $suffixRe    = "\033[38;5;198mNew Episode\033[0m";
+
 printf("%30s\n",'TV TODAY');
 printf("%30s\n", "--------" ); 
 foreach my $rel(sort(@episodes)) {
-
   chomp($rel);
-  $rel = "\033[38;5;196m$rel \033[0m" if $rel =~ /$reNew/;
-  $rel = "\033[38;5;114m       $rel\033[0m" if $rel =~ /$reDocu/i;
-  $rel = "\033[38;5;245mSPORT\033[0m: $rel" if $rel =~ /$reSport/i;
-  $rel = "\033[38;5;104m  SWE\033[0m: $rel" if $rel =~ /$reSwe/i;
-  $rel = "\033[38;5;208m$rel\033[0m" if $rel =~ /$reWanted/i;
-  $rel = "\033[38;5;131m$rel \033[0m" if $rel !~ /$reRecurring/i;
-  $rel = "       $rel" if $rel !~ /$reNew|$reDocu|$reSport|$reSwe/i;
+  $rel = sprintf("%55s", $rel);
+  $rel = sprintf("%.55s",$rel);
+  $rel = sprintf("%s $suffixRe", $rel) if $rel =~ /$reRecurring/ix;
+  $rel = sprintf("%s $suffixDocu", $rel) if $rel =~ /$reDocu/ix;
+  $rel = sprintf("%s $suffixSwe", $rel) if $rel =~ /$reSwe/ix;
+  $rel = sprintf("%s $suffixSport", $rel) if $rel =~ /$reSport/ix;
+  $rel = sprintf("%s $suffixNew", $rel) if $rel =~ /$reNew/ix;
+  $rel = sprintf("$colorWanted%s$colorNone",$rel) if $rel =~ /$reWanted/i;
+
 
   print $rel, "\n";
 }
