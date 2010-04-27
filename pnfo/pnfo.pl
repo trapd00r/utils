c13d9e68eafdcc06aa93e1875613ec9a44ffe7ce
trapd00r <trapd00r@trapd00r.se>
2010-04-27 13:17:59 +0200

    pnfo: changed options a bit to match the neverland vim theme

--- pnfo/pnfo.pl
+++ pnfo/pnfo.pl
@@ -4,7 +4,7 @@ use strict;
 # NFO files are ASCII-art with the cp437 codepage
 # Lucida ConsoleP has support for all the cp437 chars
 
-my $term = "urxvt -fn xft:'Lucida ConsoleP' +sb -uc";
+my $term = "urxvt -fn xft:'Lucida ConsoleP' -fg '#ffffff' -uc +sb";
 my $viewer = "vimpager-nfo";
 if($ARGV[0] =~ /\.nfo$/) {
         system("$term -e $viewer $ARGV[0]");
