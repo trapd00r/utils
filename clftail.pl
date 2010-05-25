5e3300cd279a424d2adad7d044253434b860e3d7
trapd00r <trapd00r@trapd00r.se>
2010-05-25 06:38:19 +0200

    clftail: update

--- clftail.pl
+++ clftail.pl
@@ -32,10 +32,21 @@ while(defined($line=$tail->read)) {
   $code = "\033[38;5;208m$code\033[0m" if $code == 301;
   $code = "\033[38;5;124m$code\033[0m" if $code == 403;
   $code = "\033[38;5;113m$code\033[0m" if $code == 304;
-  $file = "" if $file =~ /^\/$/;
-  $file = "\033[38;5;160m$file\033[0m" if $file !~ /([A-Za-z0-9])+/;
+  $request = "\033[38;5;190m    $request\033[0m" if $request eq 'GET';
+  $request = "\033[38;5;196m   $request\033[0m" if $request eq 'POST';
+  $request = "\033[38;5;197m$request\033[0m" if $request eq 'CONNECT';
+
+  if($file =~ /\.(:?png|gif|jpg|jpeg)$/) {
+    $file = "\033[38;5,167m$file\033[0m";
+  }
+  elsif($file =~ /\.html$/) {
+    $file = "\033[38;5;74m$file\033[0m";
+  }
+  else {
+    $file = "\033[38;5;222m$file\033[0m";
+  }
+
   $size = "\033[38;5;160m$size\033[0m" if $size > 5;
-  printf("%s %7s %s %s %60s\n",
+  printf("%s %7s %s \033[38;5;215m%s\033[0m \t%-60.40s\n",
   $code, $request, $size, $ip, $file);
 }
-
