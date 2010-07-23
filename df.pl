70cc89ba654d6db7b7d863c171a8c93420b782e5
trapd00r <trapd00r@trapd00r.se>
2010-07-23 14:30:26 +0200

    df.pl: visually show percent used in gradually hotter color

--- df.pl
+++ df.pl
@@ -40,14 +40,41 @@ for my $l(@c) {
 my $dpf = undef;
 for my $dp(sort (keys(%disks))) {
   if($dp =~ m;([a-z]+)([0-9]+);) {
-    $dpf = "\033[38;5;148m$1\033[38;5;178m\033[1m$2";
+    $dpf = "\033[38;5;148m$1\033[38;5;178m\033[1m$2\033[0m";
   }
 
+  my $proc = $disks{$dp}{proc};
+  my $percent = undef;
+  for($proc) {
+    m/([0-9]+)%/;
+    $percent = $1;
+    if($percent > 90) {
+      $percent = "\033[38;5;196m$percent\033[0m";
+    }
+    elsif($percent > 75) { 
+      $percent = "\033[38;5;202m$percent\033[0m";
+    }
+    elsif($percent > 50) {
+      $percent = "\033[38;5;208m$percent\033[0m";
+    }
+    elsif($percent > 25) {
+      $percent = "\033[38;5;226m$percent\033[0m";
+    }
+    elsif($percent < 25) {
+      $percent = "\033[38;5;148m$percent\033[0m";
+    }
+    elsif($percent < 10) {
+      $percent = "\033[38;5;082m afafs $percent\033[0m";
+    }
+    else {
+      $percent = "\033[38;5;244m$percent\033[0m";
+    }
+  }
 
-  printf("\033[1m%5s\033[0m \033[38;5;29m%s \033[38;5;244m %4s \033[38;5;202m%4s \033[38;5;192m%4s \033[38;5;148m%3s <\033[38;5;178m\033[1m%s \033[0m\n", 
+  printf("\033[1m%6s\033[0m \033[38;5;29m%s \033[38;5;244m %4s \033[38;5;202m%4s \033[38;5;192m%4s \033[38;5;148m%17s\033[0m% <\033[38;5;191m\033[1m%s \033[0m\n", 
     $disks{$dp}{fs}, $dpf,
     $disks{$dp}{size}, $disks{$dp}{used},
-    $disks{$dp}{avail},$disks{$dp}{proc},
+    $disks{$dp}{avail},$percent,
     $disks{$dp}{mount}
-  ) unless(!defined($disks{$dp}));
+  ) unless(!defined($disks{$dp}{mount}));
 }
