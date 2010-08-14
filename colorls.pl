d1837edb6150262d110e8c9aaf22b03ee22b0346
trapd00r <trapd00r@trapd00r.se>
2010-08-14 20:33:59 +0200

    -x

--- colorls.pl
+++ colorls.pl
@@ -10,6 +10,7 @@ my $CPIPE = "\033[38;5;197m";
 
 @ARGV = './' unless  @ARGV;
 
+# -halv ?
 my $ls = "ls -hAov --indicator-style=file-type --color=always --group-directories-first --time=ctime \"@ARGV\"";
 open(my $ph, "-|", $ls) or die $!;
 
@@ -20,7 +21,8 @@ my %seen;
 for(@arr) {
   ++$i;
   next if(/^total/);
-  m/^([drwxsT-]+)\s+([0-9]+)\s+([A-Za-z0-9]+)\s+([0-9KMGT\.]+)\s+([A-Za-z]{3})\s+([0-9]{1,3})\s+([0-9:]+)(.+)$/;
+  #s/->/▪▶/g;
+  m/^([drwxsT-]+)\s+([0-9]+)\s+([A-Za-z0-9]+)\s+([0-9KMGT\.]+)\s+([A-Za-z]{3})\s+([0-9]{1,3})\s+([0-9:]+)(.+)(->)?(.+)?$/;
 
   my $perm = $1;
   my $fno  = $2;
@@ -30,6 +32,8 @@ for(@arr) {
   my $dinmon = $6;
   my $time = $7;
   my $file = $8;
+  my $sym = $9;
+  my $sym_t = $10;
 
   $seen{$file}++;
   next if($seen{$file} > 1);
@@ -38,6 +42,7 @@ for(@arr) {
 
   printf("$CPERM%s$CDIM%3d\033[0m $CDATE%2s\033[0m $CFNO%s\033[0m$CPIPE▕\033[0m%s\n",
     $perm, $dinmon,$month,sprintf("%4d",$fno), $file);
+  print $sym,$sym_t;
 }
 print "\033[38;5;160m$i\033[0m Files\n";
 
