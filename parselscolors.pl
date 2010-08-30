54a2fb4b1fd42a4a7f029cd63cd8a68cdd1c52b3
trapd00r <trapd00r@trapd00r.se>
2010-08-30 07:58:57 +0200

    parselscolors: rewrite

--- parselscolors.pl
+++ parselscolors.pl
@@ -1,28 +1,18 @@
 #!/usr/bin/perl
-# parselscolors
 use strict;
 
-my $lscolors = "$ENV{LS_COLORS}";
+my $colors = $ENV{LS_COLORS};
 
-my @ft = split(/=/, $lscolors);
-
-my %colors = ();
-my $symlink;
-
-for my $ft(@ft) {
-  if($ft =~ m/([0-9]+;[0-9]+;[0-9]+):\*?(.+)/) {
-  my $clr = $1;
-  my $file = $2;
-  $colors{$file} = $clr;
-}
- 
-  if($ft =~ m/(target|follow)/) {
-    next;
+my $i=0;
+my $end;
+for(split(/:/,$colors)) {
+  if($i % 22 == 0) {
+    $end = "\n";
   }
+  else {
+    $end = " ";
+  }
+  my ($ft,$color) = $_ =~ /\*\.(.+)=(.+)/;
+  print "\e[$color" . 'm' . "$ft\e[0m$end";
+  $i++;
 }
-
-for my $f(sort(keys(%colors))) {
-  printf("\033[$colors{$f}m %25s\033[0m %s\n",
-    "$f", $colors{$f});
-}
-printf(" %25s %s\n",'symlink',$symlink); # bah, lazy
