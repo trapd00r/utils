3f4ef5a68f7546712d5de5a6c3d7fb2c681e5caf
trapd00r <trapd00r@trapd00r.se>
2010-08-30 06:51:13 +0200

    iterate: rewrite, eval perl code

--- iterate.pl
+++ iterate.pl
@@ -1,10 +1,37 @@
 #!/usr/bin/perl
 use strict;
+use Getopt::Long;
 
-my $count = shift or die();
-my $cmd = sub { system(@ARGV) };
+usage() if(!@ARGV);
 
-for(my $i=0; $i < $count; ++$i) {
-  print "run ".$i+1;
-  $cmd->();
+our ($opt_count, $opt_perl) = (10, 0);
+GetOptions(
+  'count:i' => \$opt_count,
+  'perl'    => \$opt_perl,
+);
+
+my @cmd = @ARGV;
+iterate($opt_count, @cmd);
+
+sub iterate {
+  my $count = shift;
+  my $cmd   = join(/ /,@_);
+
+  my $i;
+  for($i=0; $i<$count; ++$i) {
+    if($opt_perl) {
+      print(eval($cmd));
+      print "\n";
+      print $@;
+    }
+    else {
+      system("$cmd");
+    }
+  }
+  return(0);
+}
+
+sub usage {
+  printf("iterate -c count (-pe perl code | command)\n");
+  exit(0);
 }
