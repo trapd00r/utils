33445e178f3b8d34202706371849cf2ece77c3a6
trapd00r <trapd00r@trapd00r.se>
2010-09-05 01:59:21 +0200

    parselscolors: rewrite

--- parselscolors.pl
+++ parselscolors.pl
@@ -1,18 +1,27 @@
 #!/usr/bin/perl
 use strict;
 
-my $colors = $ENV{LS_COLORS};
+my $c = $ENV{LS_COLORS};
 
-my $i=0;
-my $end;
-for(split(/:/,$colors)) {
-  if($i % 22 == 0) {
-    $end = "\n";
+my $i = 0;
+for(split(/:/, $ENV{LS_COLORS})) {
+  my($ft,$color) = $_ =~ /\*\.(.+)=(.+)/;
+  if(@ARGV) {
+    my $end;
+    if($i % 6 == 0) {
+      $end = "\n";
+    }
+    else {
+      $end = "\t";
+    }
+    printf("\e[$color%s %10s\e[0m $end", 'm', $ft) 
+      unless((!$ft)
+          or($ft =~ /^(?:symlink|ow|tw|di|fi|ln|do|pi|bd|cd|su|sg|st|ex|mi|lc|rc|ec)$/));
+    $i++;
   }
   else {
-    $end = " ";
+    open(my $fh, '>', ".$ft");
+    close($fh);
   }
-  my ($ft,$color) = $_ =~ /\*\.(.+)=(.+)/;
-  print "\e[$color" . 'm' . "$ft\e[0m$end";
-  $i++;
 }
+print "\n";
