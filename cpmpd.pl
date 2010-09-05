84b0848e610680d7e192fd119793b267e4c2df3f
trapd00r <trapd00r@trapd00r.se>
2010-09-05 23:26:37 +0200

    cpmpd.pl: Since I need all music files in the root of the sansa, lets use grep and find on the remote box (probably the ugliest hack ever) and iterate the array of files one by one

--- cpmpd.pl
+++ cpmpd.pl
@@ -1,12 +1,14 @@
 #!/usr/bin/perl
+# cpmpd
+# copy now playing track or album from remote MPD server to local box
 use strict;
 use Getopt::Long;
 
-my $base   = '/mnt/Music_1';
-my $target = "$ENV{HOME}/ToTransfer";
-my $host   = '192.168.1.101';
+my $base       = '/mnt/Music_1';
+my $target     = "$ENV{HOME}/ToTransfer";
+my $host       = '192.168.1.101';
 chomp(my $file = `mpc -h $host --format %file%|head -1`);
-my $path   = "$base/$file";
+my $path       = "$base/$file";
 my ($basename) = $path =~ m;.+/(.+)$;;
 my ($album)    = $path =~ m;(.+)/.+$;;
 
@@ -23,7 +25,13 @@ sub transfer {
     exec('scp', "-P 19216", "scp1\@$host:\"$path\"", $target);
   }
   else {
-    exec('scp', '-P 19216', '-r', "scp1\@$host:\"$album\"", $target);
+    # I need all music files in the root of my music player
+    chomp(my @files = grep{/\.(?:mp3|flac)$/} `ssh -p 19216 scp1\@$host find $album`);
+    map{$_ = "'$_'"} @files;
+
+    for(@files) {
+      system("scp -P 19216 scp1\@$host:$_ $target");
+    }
   }
   exit(0);
 }
