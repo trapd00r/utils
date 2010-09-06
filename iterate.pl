130c20d9b0ffaba5e30d049b035fa424d9e25556
trapd00r <trapd00r@trapd00r.se>
2010-09-06 10:05:16 +0200

    iterate: made multiword commands possible without escaping. Fetch input from STDIN if perl code is to be executed (shell will eat $_,$0,$$...)

--- iterate.pl
+++ iterate.pl
@@ -1,29 +1,38 @@
 #!/usr/bin/perl
 use strict;
 use Getopt::Long;
+use Pod::Usage;
 
 usage() if(!@ARGV);
 
-our ($opt_count, $opt_perl) = (10, 0);
+our ($opt_count, $opt_perl, $opt_newline) = (10, 0, 0);
 GetOptions(
   'count:i' => \$opt_count,
   'perl'    => \$opt_perl,
+  'newline' => \$opt_newline,
+  'help'    => sub { pod2usage(verbose => 1); },
 );
 
-my ($torun) = @ARGV;
+my $torun = shift // undef;
+
 iterate($opt_count, $torun);
 
 sub iterate {
   my $count = shift;
-  my $cmd = shift;
+  my $cmd = join(' ', @_);
 
   my $i;
+  my @foo;
+  if($opt_perl) {
+    print "\e[1m\e[36m> ";
+    chomp($cmd = <STDIN>);
+    print "\e[0m";
+  }
   for($i=0; $i<$count; ++$i) {
     if($opt_perl) {
       my $foo = (eval($cmd));
       if($@) {
-        # :D
-        system("$cmd")==0 or print"$cmd: \e[1mJUNK args\e[0m\n" and exit(-1);
+        print $@;
       }
       else {
         print $foo, "\n";
@@ -31,6 +40,7 @@ sub iterate {
     }
     else {
       system("$cmd");
+      print "\n" unless(!$opt_newline);
     }
   }
   return(0);
@@ -40,3 +50,14 @@ sub usage {
   printf("iterate -c count (-pe perl code | command)\n");
   exit(0);
 }
+
+=pod
+
+=head1 OPTIONS
+
+  -c,   --count   number of iterations
+  -n,   --newline append newline to every line (default: OFF)
+  -p,   --perl    evaluate perl code
+  -h,   --help    show help and exit
+
+=cut
