41e74f1aa25514ab582516429cb3651bcd5f5979
trapd00r <trapd00r@trapd00r.se>
2010-09-06 07:29:30 +0200

    cpmpd.pl: rewrite. Made it somewhat useful for others. Implemented single track and full album transfers, implemeted search-and-grab function

--- cpmpd.pl
+++ cpmpd.pl
@@ -1,38 +1,86 @@
 #!/usr/bin/perl
-# cpmpd
-# copy now playing track or album from remote MPD server to local box
+our $APP     = 'cpmpd';
+our $VERSION = 0.2;
+
 use strict;
 use Getopt::Long;
 
+#FIXME throw this crap in a config file
 my $base       = '/mnt/Music_1';
 my $target     = "$ENV{HOME}/ToTransfer";
+my $user       = 'scp1';
 my $host       = '192.168.1.101';
+my $port       = 19216;
+my $scp        = '/usr/bin/scp';
+
 chomp(my $file = `mpc -h $host --format %file%|head -1`);
 my $path       = "$base/$file";
+
 my ($basename) = $path =~ m;.+/(.+)$;;
 my ($album)    = $path =~ m;(.+)/.+$;;
+my ($albname)  = $album =~ m;.+/(.+)$;;
 
-our($opt_album) = 0;
+our($opt_album) = undef;
+our(@opt_search);
+
 GetOptions(
-  'album'    => \$opt_album,
-  'target:s' => \$target,
+  'album'         => \&get_album,
+  'track'         => \&get_track,
+  'dest:s'        => \$target,
+  'search=s{1,}'  => \@opt_search,
 );
 
-transfer($opt_album);
-sub transfer {
-  my $what = shift;
-  if($what == 0) {
-    exec('scp', "-P 19216", "scp1\@$host:\"$path\"", $target);
-  }
-  else {
-    # I need all music files in the root of my music player
-    chomp(my @files = grep{/\.(?:mp3|flac)$/} `ssh -p 19216 scp1\@$host \"find \'$album\'\"`);
-    map{$_ = "'$_'"} @files;
+search(@opt_search) if(@opt_search);
 
-    for(@files) {
-      $_ =~ s/([;<>\*\|`&\$!#\(\)\[\]\{\}:'"])/\\$1/g;
-      system("scp -P 19216 scp1\@$host:$_ $target");
-    }
+sub get_album {
+  printf("\e[38;5;208m\e[1m$albname\e[0m => \e[1m$target\e[0m\n");
+  transfer($album);
+}
+sub get_track {
+  printf("\e[38;5;196m\e[1m$basename\e[0m => \e[1m$target\e[0m\n");
+  transfer($path);
+}
+
+sub search {
+  my @search = @_;
+  my $query = join(' ', @search);
+  chomp(my @result = `mpc -h $host search $query`);
+  if(@result) {
+    map{$_ = "$base/$_"} @result;
+    transfer(@result);
   }
   exit(0);
 }
+sub transfer {
+  my @files = @_;
+  for(@files) {
+    $_ =~ s/([;<>\*\|`&\$!#\(\)\[\]\{\}:'"])/\\$1/g;
+    system("$scp -P $port -r $user\@$host:\"$_\" $target");
+  }
+  exit(0);
+}
+
+=pod
+
+=head1 NAME
+
+  cpmpd - copy music from a (remote) MPD server
+
+=head1 DESCRIPTION
+
+  cpmpd is cool
+
+=head1 OPTIONS
+
+  --dest    set target destination
+  --album   copy the now playing album to target
+  --track   copy the now playing track to target
+  --search  search the MPD db and copy results to target
+
+=head1 COPYRIGHT
+
+Copyright (C) Magnus Woldrich 2010
+
+License GPLv2
+
+=cut
