32fd15436bdf09cbb304e601f78f21b32b363369
Magnus Woldrich <trapd00r@trapd00r.se>
2010-11-01 09:32:00 +0100

    clftail.pl: cleanup

--- clftail.pl
+++ clftail.pl
@@ -26,27 +26,27 @@ while(defined($line=$tail->read)) {
   my $code    = $10;
   my $size    = $11;
 
-  $code = "\033[38;5;240m$code\033[0m" if $code == 404;
-  $code = "\033[38;5;155m$code\033[0m" if $code == 200;
-  $code = "\033[38;5;160m$code\033[0m" if $code == 501;
-  $code = "\033[38;5;208m$code\033[0m" if $code == 301;
-  $code = "\033[38;5;124m$code\033[0m" if $code == 403;
-  $code = "\033[38;5;113m$code\033[0m" if $code == 304;
-  $request = "\033[38;5;190m    $request\033[0m" if $request eq 'GET';
-  $request = "\033[38;5;196m   $request\033[0m" if $request eq 'POST';
-  $request = "\033[38;5;197m$request\033[0m" if $request eq 'CONNECT';
+  $code = "\e[38;5;240m$code\e[0m" if $code == 404;
+  $code = "\e[38;5;155m$code\e[0m" if $code == 200;
+  $code = "\e[38;5;160m$code\e[0m" if $code == 501;
+  $code = "\e[38;5;208m$code\e[0m" if $code == 301;
+  $code = "\e[38;5;124m$code\e[0m" if $code == 403;
+  $code = "\e[38;5;113m$code\e[0m" if $code == 304;
+  $request = "\e[38;5;190m    $request\e[0m" if $request eq 'GET';
+  $request = "\e[38;5;196m   $request\e[0m" if $request eq 'POST';
+  $request = "\e[38;5;197m$request\e[0m" if $request eq 'CONNECT';
 
   if($file =~ /\.(:?png|gif|jpg|jpeg)$/) {
-    $file = "\033[38;5,167m$file\033[0m";
+    $file = "\e[38;5,167m$file\e[0m";
   }
   elsif($file =~ /\.html$/) {
-    $file = "\033[38;5;74m$file\033[0m";
+    $file = "\e[38;5;74m$file\e[0m";
   }
   else {
-    $file = "\033[38;5;222m$file\033[0m";
+    $file = "\e[38;5;222m$file\e[0m";
   }
 
-  $size = "\033[38;5;160m$size\033[0m" if $size > 5;
-  printf("%s %7s %s \033[38;5;215m%s\033[0m \t%-60.40s\n",
+  $size = "\e[38;5;160m$size\e[0m" if $size > 5;
+  printf("%s %7s %s \e[38;5;215m%s\e[0m \t%-60.40s\n",
   $code, $request, $size, $ip, $file);
 }
