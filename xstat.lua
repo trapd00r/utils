ee6c068500d22f57661df8643670b37be9b0dd00
Magnus Woldrich <trapd00r@trapd00r.se>
2011-01-03 08:39:15 +0100

    xstat.lua: added command line parsing and dzen function

--- xstat.lua
+++ xstat.lua
@@ -1,7 +1,10 @@
 #!/usr/bin/lua
 
--- xstat v0.01
+-- xstat v0.02
 -- reports total amount of sent/recieved traffic in megabytes.
+
+local app     = "xstat";
+local version = 0.02;
 
 function printf(s,...)
   return io.write(s:format(...))
@@ -23,9 +26,37 @@ function get_stats()
   return tx_mb, rx_mb;
 end
 
+
+function to_tty(s, r) 
+  printf("%8s: %4d MB\n%s: %4d MB\n",
+    "Sent",     s,
+    "Recieved", r
+  );
+end
+
+function to_dzen(s, r)
+  io.write("^fg(#ff0000)Sent^fg(): ", s,
+    "^fg(#ffff00)Recieved^fg(): ", r
+    );
+end
+
+function usage(app, ver)
+  printf("%s v%.2f\n\nUsage: %s [-t -d -h]\n", app, ver, app);
+end
+
+
+if(argv == 0) then
+  usage(app, version);
+end
+
 local sent, recieved = get_stats();
 
-printf("%8s: %d MB\n%s: %d MB\n\n",
-  "Sent",     sent,
-  "Recieved", recieved
-);
+if(arg[1] == '-d') then
+  to_dzen(sent, recieved);
+elseif(arg[1] == '-t') then
+  to_tty(sent, recieved);
+elseif(arg[1] == '-h') then
+  usage(app, version);
+else
+  to_tty(sent, recieved);
+end
