5e159943f4b11f0d57373f395a2f6c0f4c6e09ea
Magnus Woldrich <m@japh.se>
2018-12-18 15:37:53 +0100

    clone_all_repos: add GITHUB_USER env var and auto-clone with xargs

--- gh_clone_all_user_repos.sh
+++ gh_clone_all_user_repos.sh
@@ -1,7 +1,10 @@
-CNTX='users'; NAME='trapd00r';
+CNTX='users'; NAME=$GITHUB_USER
+
+# 189 repos as of  2018-12-18 15:34:34 
 
 for PAGE in {1..3}; do 
   curl -s "https://api.github.com/$CNTX/$NAME/repos?page=$PAGE&per_page=100" |
     grep -e 'git_url*' |
-    cut -d \" -f 4 ;
+    cut -d \" -f 4  |
+    xargs -L1 git clone
   done
