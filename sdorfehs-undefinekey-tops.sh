746d3076491ad2f691535e14c4d6ec61f07725d0
Magnus Woldrich <magnus.woldrich@gmail.com>
2021-11-29 13:26:26 +0100

    sdorfehs: add undefinekey script for switching virtuals

---
+++ sdorfehs-undefinekey-tops.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# abstract: undefine keys for switching workspaces
+sdorfehs -c 'undefinekey top s-1'
+sdorfehs -c 'undefinekey top s-2'
+sdorfehs -c 'undefinekey top s-3'
+sdorfehs -c 'undefinekey top s-4'
+sdorfehs -c 'undefinekey top s-5'
+sdorfehs -c 'undefinekey top s-6'
+sdorfehs -c 'undefinekey top s-7'
+sdorfehs -c 'undefinekey top s-8'
+sdorfehs -c 'undefinekey top s-9'
+sdorfehs -c 'undefinekey top s-0'
+sdorfehs -c 'undefinekey top s-q'
+sdorfehs -c 'undefinekey top s-w'
+sdorfehs -c 'undefinekey top s-e'
+
