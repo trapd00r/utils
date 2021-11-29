a36bf601c0179a44c924fde34eec4cfd7966b1c4
Magnus Woldrich <magnus.woldrich@gmail.com>
2021-11-29 13:26:10 +0100

    sdorfehs: add definekey script for switching virtuals

---
+++ sdorfehs-definekey-tops.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# abstract: define keys for switching workspaces
+
+sdorfehs -c 'definekey top s-1 vselect 0'
+sdorfehs -c 'definekey top s-2 vselect 1'
+sdorfehs -c 'definekey top s-3 vselect 2'
+sdorfehs -c 'definekey top s-4 vselect 3'
+sdorfehs -c 'definekey top s-5 vselect 4'
+sdorfehs -c 'definekey top s-6 vselect 5'
+sdorfehs -c 'definekey top s-7 vselect 6'
+sdorfehs -c 'definekey top s-8 vselect 7'
+sdorfehs -c 'definekey top s-9 vselect 8'
+sdorfehs -c 'definekey top s-0 vselect 9'
+sdorfehs -c 'definekey top s-q vselect 10'
+sdorfehs -c 'definekey top s-w vselect 11'
+sdorfehs -c 'definekey top s-e vselect 12'
