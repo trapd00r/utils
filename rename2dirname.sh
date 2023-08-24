4604626b64a9b664dc52117619159350f6b4fb01
Magnus Woldrich <magnus.woldrich@gmail.com>
2023-08-24 06:58:19 +0200

    rename2dirname.sh: make it easier to rename to final filenames with vidir

--- rename2dirname.sh
+++ rename2dirname.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-# rename files in subdirs to the name of the subdir_originalfilename
-for f in */* ;do fp=$(dirname "$f"); fl=$(basename "$f"); mv -v "$fp/$fl" "$fp/$fp"_"$fl"; done
+# rename files in subdirs to the name of the subdir_____originalfilename
+for f in */* ;do fp=$(dirname "$f"); fl=$(basename "$f"); mv -v "$fp/$fl" "$fp/$fp"_____"$fl"; done
